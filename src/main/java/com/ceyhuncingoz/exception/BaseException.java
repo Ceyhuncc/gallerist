f428c8e1d91cf7231ed7cfe8979e8df3f2e928af
LST\ceyhun.cingoz <ceyhun.cingoz@pro-line.com.tr>
2025-09-25 17:18:22 +0300

    Exception handler

---
+++ src/main/java/com/ceyhuncingoz/exception/BaseException.java
@@ -0,0 +1,9 @@
+package com.ceyhuncingoz.exception;
+
+
+public class BaseException extends RuntimeException {
+
+    public BaseException(ErrorMessage errorMessage){
+        super(errorMessage.prepareErrorMessage());
+    }
+}
